a52669bd408259b95f9499f38d1e3c7beb33348f
goofiw <goofiw@gmail.com>
2015-10-14 19:42:40 -0700

    mvp

--- routes/router.js
+++ routes/router.js
@@ -2,18 +2,22 @@
 var members = require('../controllers/members.js');
 var router = require('koa-router')();
 
-  router.get('/api/members', members.auth, members.allmembers),
+  router.get('/api/members', members.auth, members.allmembers);
 
-  router.post('/api/addmembers', members.auth, members.addMembers),
+  router.post('/api/addmembers', members.auth, members.addMembers);
 
   router.post('/api/addmember', function *(next){
     //accepts one member
   }),
 
+  router.post('/api/notify', members.notify);
+
+
   router.post('/api/fileupload', function *(next) {
     console.log(this);
     yield this.body = {};
   }),
+
 
   router.get('/api/awsurl', function *(next){
     yield this.body = {url: upload.getUrl()};
